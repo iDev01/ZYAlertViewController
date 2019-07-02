a26036eb755ab30cdb17b21e15266b100230a14d
马浩 <mahao@smodock.com>
2019-07-02 16:34:12 +0800

    示例程序

--- Example/Example/ViewController.m
+++ Example/Example/ViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "ViewController.h"
-#import "ZYAlertViewController.h"
+#import <ZYAlertViewController/ZYAlertViewController.h>
 
 @interface ViewController ()
 
@@ -19,16 +19,31 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
 
-    UIButton *button = [[UIButton alloc] initWithFrame:CGRectMake(50, 50, 90, 40)];
-    [button setTitle:@"点我弹Alert" forState:UIControlStateNormal];
-    [button addTarget:self action:@selector(clicked:) forControlEvents:UIControlEventTouchUpInside];
-    [self.view addSubview:button];
+    UIButton *vButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    vButton.frame = CGRectMake(50, 50, 200, 40);
+    [vButton setTitle:@"下方弹出Alert" forState:UIControlStateNormal];
+    [vButton setTitleColor:UIColor.blackColor forState:UIControlStateNormal];
+    [vButton addTarget:self action:@selector(vButtonClicked:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:vButton];
+    UIButton *dButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    dButton.frame = CGRectMake(50, 100, 200, 40);
+    [dButton setTitle:@"溶解弹出Alert" forState:UIControlStateNormal];
+    [dButton setTitleColor:UIColor.blackColor forState:UIControlStateNormal];
+    [dButton addTarget:self action:@selector(dButtonClicked:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:dButton];
 }
 
-- (void)clicked:(id)sender {
-    UIView *view = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 300, 200)];
+- (void)vButtonClicked:(id)sender {
+    UIView *view = [[UIView alloc] initWithFrame:CGRectMake(37.5, 500, 300, 200)];
     view.backgroundColor = [UIColor brownColor];
     ZYAlertViewController *alertVC = [[ZYAlertViewController alloc] initWithView:view presentDirection:PresentDirectionCoverVertical];
+    [self presentViewController:alertVC animated:YES completion:nil];
+}
+
+- (void)dButtonClicked:(id)sender {
+    UIView *view = [[UIView alloc] initWithFrame:CGRectMake(37.5, 160, 300, 200)];
+    view.backgroundColor = [UIColor brownColor];
+    ZYAlertViewController *alertVC = [[ZYAlertViewController alloc] initWithView:view presentDirection:PresentDirectionCrossDissolve];
     [self presentViewController:alertVC animated:YES completion:nil];
 }
 
