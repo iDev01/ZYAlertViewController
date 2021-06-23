2022d2a00e0414a93b7e65942ac0ceb2d5f823a2
iDev_01 <ideveloper_mahao@smodock.com>
2021-06-23 19:59:30 +0800

    支持autolayout

--- ZYAlertViewController/ZYAlertViewController.m
+++ ZYAlertViewController/ZYAlertViewController.m
@@ -12,7 +12,6 @@
 
 @property (assign, nonatomic) PresentDirection presentDirection;
 @property (strong, nonatomic) UIViewController *toViewController;
-@property (assign, nonatomic) BOOL dismissWhenTap;
 
 @end
 
@@ -26,19 +25,20 @@
 
 @property (strong, nonatomic) PresentAnimator *presentAnimator;
 @property (strong, nonatomic) DismissAnimator *dismissAnimator;
-
+@property (assign, nonatomic) PresentDirection presentDirection;
+@property (strong, nonatomic) UIView *contentView;
 @end
 
 @implementation ZYAlertViewController
 
-- (instancetype)initWithView:(UIView *)view presentDirection:(PresentDirection)presentDirection dismissWhenTap:(BOOL)dismissWhenTap {
+- (instancetype)initWithView:(UIView *)view presentDirection:(PresentDirection)presentDirection {
     if (self = [super init]) {
-        self.view = view;
+        self.contentView = view;
         self.modalPresentationStyle = UIModalPresentationCustom;
         self.transitioningDelegate = self;
+        self.presentDirection = presentDirection;
         self.presentAnimator = [PresentAnimator new];
         self.presentAnimator.presentDirection = presentDirection;
-        self.presentAnimator.dismissWhenTap = dismissWhenTap;
         self.dismissAnimator = [DismissAnimator new];
         self.dismissAnimator.presentDirection = presentDirection;
     }
@@ -48,7 +48,50 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-
+    [self.view addSubview:self.contentView];
+    self.view.translatesAutoresizingMaskIntoConstraints = NO;
+    NSLayoutConstraint *widthGreaterThanOrEqualLayoutConstraint = [NSLayoutConstraint constraintWithItem:self.view
+                                                                                               attribute:NSLayoutAttributeWidth
+                                                                                               relatedBy:NSLayoutRelationGreaterThanOrEqual
+                                                                                                  toItem:self.contentView
+                                                                                               attribute:NSLayoutAttributeWidth
+                                                                                              multiplier:1
+                                                                                                constant:0];
+    NSLayoutConstraint *widthEqualLayoutConstraint = [NSLayoutConstraint constraintWithItem:self.view
+                                                                                  attribute:NSLayoutAttributeWidth
+                                                                                  relatedBy:NSLayoutRelationEqual
+                                                                                     toItem:self.contentView
+                                                                                  attribute:NSLayoutAttributeWidth
+                                                                                 multiplier:1
+                                                                                   constant:0];
+    widthEqualLayoutConstraint.priority = 49;
+    NSLayoutConstraint *heightLayoutConstraint = [NSLayoutConstraint constraintWithItem:self.view
+                                                                              attribute:NSLayoutAttributeHeight
+                                                                              relatedBy:NSLayoutRelationEqual
+                                                                                 toItem:self.contentView
+                                                                              attribute:NSLayoutAttributeHeight
+                                                                             multiplier:1
+                                                                               constant:0];
+    NSLayoutConstraint *centerXGreaterThanOrEqualLayoutConstraint = [NSLayoutConstraint constraintWithItem:self.contentView
+                                                                                               attribute:NSLayoutAttributeCenterX
+                                                                                               relatedBy:NSLayoutRelationEqual
+                                                                                                  toItem:self.view
+                                                                                               attribute:NSLayoutAttributeCenterX
+                                                                                              multiplier:1
+                                                                                                constant:0];
+    NSLayoutConstraint *centerYGreaterThanOrEqualLayoutConstraint = [NSLayoutConstraint constraintWithItem:self.contentView
+                                                                                               attribute:NSLayoutAttributeCenterY
+                                                                                               relatedBy:NSLayoutRelationEqual
+                                                                                                  toItem:self.view
+                                                                                               attribute:NSLayoutAttributeCenterY
+                                                                                              multiplier:1
+                                                                                                constant:0];
+    [self.view addConstraints:@[widthGreaterThanOrEqualLayoutConstraint,
+                                widthEqualLayoutConstraint,
+                                heightLayoutConstraint,
+                                centerXGreaterThanOrEqualLayoutConstraint,
+                                centerYGreaterThanOrEqualLayoutConstraint]];
+    
 }
 
 - (id<UIViewControllerAnimatedTransitioning>)animationControllerForDismissedController:(UIViewController *)dismissed {
@@ -70,10 +113,8 @@
 - (void)animateTransition:(id<UIViewControllerContextTransitioning>)transitionContext {
     UIView *bgView = [[UIView alloc] initWithFrame:[UIScreen mainScreen].bounds];
     bgView.backgroundColor = [UIColor clearColor];
-    bgView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
-    transitionContext.containerView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
     [transitionContext.containerView addSubview:bgView];
-    if (self.dismissWhenTap) {
+    if (self.presentDirection == PresentDirectionCoverVertical) {
         UITapGestureRecognizer *tapGR = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tap:)];
         [bgView addGestureRecognizer:tapGR];
     }
@@ -81,8 +122,6 @@
     UIView *toView = [transitionContext viewControllerForKey:UITransitionContextToViewControllerKey].view;
     CGRect storeFrame = toView.frame;
     CGRect frame = toView.frame;
-    toView.center = transitionContext.containerView.center;
-    toView.autoresizingMask = UIViewAutoresizingFlexibleTopMargin | UIViewAutoresizingFlexibleRightMargin | UIViewAutoresizingFlexibleBottomMargin | UIViewAutoresizingFlexibleLeftMargin;
     switch (self.presentDirection) {
         case PresentDirectionCoverVertical:
             frame.origin.y = [UIScreen mainScreen].bounds.size.height;
@@ -95,17 +134,55 @@
             break;
     }
     [transitionContext.containerView addSubview:toView];
+
+    NSLayoutConstraint *topGreaterThanOrEqualLayoutConstraint = [NSLayoutConstraint constraintWithItem:toView
+                                                                                             attribute:NSLayoutAttributeTop
+                                                                                             relatedBy:NSLayoutRelationGreaterThanOrEqual
+                                                                                                toItem:transitionContext.containerView
+                                                                                             attribute:NSLayoutAttributeTop
+                                                                                            multiplier:1
+                                                                                              constant:47];
+    NSLayoutConstraint *bottomGreaterThanOrEqualLayoutConstraint = [NSLayoutConstraint constraintWithItem:toView
+                                                                                                attribute:NSLayoutAttributeBottom
+                                                                                                relatedBy:NSLayoutRelationLessThanOrEqual
+                                                                                                   toItem:transitionContext.containerView
+                                                                                                attribute:NSLayoutAttributeBottom
+                                                                                               multiplier:1
+                                                                                                 constant:-47];
+    NSLayoutConstraint *centerX = [NSLayoutConstraint constraintWithItem:toView attribute:NSLayoutAttributeCenterX relatedBy:NSLayoutRelationEqual toItem:transitionContext.containerView attribute:NSLayoutAttributeCenterX multiplier:1 constant:0];
+    NSLayoutConstraint *centerY;
+    switch (self.presentDirection) {
+        case PresentDirectionCoverVertical:
+            centerY = [NSLayoutConstraint constraintWithItem:toView attribute:NSLayoutAttributeBottom relatedBy:NSLayoutRelationEqual toItem:transitionContext.containerView attribute:NSLayoutAttributeBottom multiplier:1 constant:-34];
+            bottomGreaterThanOrEqualLayoutConstraint = [NSLayoutConstraint constraintWithItem:toView
+                                                                                                        attribute:NSLayoutAttributeBottom
+                                                                                                        relatedBy:NSLayoutRelationLessThanOrEqual
+                                                                                                           toItem:transitionContext.containerView
+                                                                                                        attribute:NSLayoutAttributeBottom
+                                                                                                       multiplier:1
+                                                                                                         constant:-34];
+            break;
+        case PresentDirectionCrossDissolve:
+            centerY = [NSLayoutConstraint constraintWithItem:toView attribute:NSLayoutAttributeCenterY relatedBy:NSLayoutRelationEqual toItem:transitionContext.containerView attribute:NSLayoutAttributeCenterY multiplier:1 constant:0];
+            break;
+    }
+
+    [transitionContext.containerView addConstraints:@[
+        topGreaterThanOrEqualLayoutConstraint,
+        bottomGreaterThanOrEqualLayoutConstraint,
+        centerX,
+        centerY]];
     [UIView animateWithDuration:0.25
                           delay:0
                         options:UIViewAnimationOptionCurveLinear
                      animations:^{
-                         bgView.backgroundColor = [UIColor colorWithWhite:0 alpha:0.4];
-                         toView.frame = storeFrame;
-                         toView.alpha = 1;
-                     }
+        bgView.backgroundColor = [UIColor colorWithWhite:0 alpha:0.4];
+        toView.frame = storeFrame;
+        toView.alpha = 1;
+    }
                      completion:^(BOOL finished) {
-                         [transitionContext completeTransition:finished];
-                     }];
+        [transitionContext completeTransition:finished];
+    }];
 }
 
 - (void)tap:(UITapGestureRecognizer *)tapGestureRecognizer {
